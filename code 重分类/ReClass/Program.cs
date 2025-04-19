e8ac268adabf3466ca1f77dc34122ae7033751f4
HZR0912 <2591926900@qq.com>
2025-04-19 21:20:54 +0800

    Add files via upload

---
+++ code 重分类/ReClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using ESRI.ArcGIS.esriSystem;
+
+namespace ReClass
+{
+    static class Program
+    {
+        private static LicenseInitializer m_AOLicenseInitializer = new ReClass.LicenseInitializer();
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //ESRI License Initializer generated code.
+            m_AOLicenseInitializer.InitializeApplication(new esriLicenseProductCode[] { esriLicenseProductCode.esriLicenseProductCodeEngine },
+            new esriLicenseExtensionCode[] { esriLicenseExtensionCode.esriLicenseExtensionCodeSpatialAnalyst });
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmReClass());
+            //ESRI License Initializer generated code.
+            //Do not make any call to ArcObjects after ShutDownApplication()
+            m_AOLicenseInitializer.ShutdownApplication();
+        }
+    }
+}
